18706f97287d93afb253ac10efc316210d9c1f82
Md Razwanul Ghani <razwanulghani@gmail.com>
2024-11-15 14:30:11 +0600

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.layouts.master');
 });
 
 Route::middleware(['auth'])->group(function () {
